969a19ea19533084cfe3ef9c65449990579e1d2d
Antonio <antonio.galaz.aroca@gmail.com>
2019-03-11 15:24:03 -0300

    Commit Trabajo

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get('/', function(req, res) {
-    res.send('Hello World')
+    res.send('Hello World rest server')
 })
 
 app.post('/usuario', function(req, res) {
