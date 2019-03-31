0819705aba75998f372a31531ac8666424fc5c8e
Antonio Galaz <antonio.galaz.aroca@gmail.com>
2019-03-31 10:12:17 -0300

    base Cafe

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://nonoeselnino:yashiro69@cluster0-ac4jn.mongodb.net/test?retryWrites=true'
+    urlDB = 'mongodb+srv://nonoeselnino:yashiro69@cluster0-ac4jn.mongodb.net/cafe?retryWrites=true'
 }
 
 process.env.URLDB = urlDB
